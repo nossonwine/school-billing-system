bae84c9b155d111bc33eb24d87fe4e08d24b8e72
nossonwine <nossonwine@gmail.com>
2026-04-16 01:22:10 -0400

    Update next.config.ts
    Ignore canvas in next config

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
 import type { NextConfig } from "next";
 
-export default {} as NextConfig;
+export default {
+  // This tells Vercel's builder to completely ignore the missing 'canvas' tool
+  serverExternalPackages: ["canvas"],
+} as NextConfig;
